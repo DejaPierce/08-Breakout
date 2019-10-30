113719328c15ce11ac4b7998bcea3a72f814bcf8
DejaPierce <54558526+DejaPierce@users.noreply.github.com>
2019-10-29 22:25:15 -0400

    Breakout

--- Scripts/Tile.gd
+++ Scripts/Tile.gd
@@ -2,7 +2,6 @@ extends StaticBody2D
 
 var gray = preload("res://Assets/tile_gray.png")
 var red = preload("res://Assets/tile_red.png")
-var blue = preload("res://Assets/tile_blue.png")
 var green = preload("res://Assets/tile_green.png")
 var purple = preload("res://Assets/tile_purple.png")
 var yellow = preload("res://Assets/tile_yellow.png")
@@ -16,9 +15,6 @@ func _ready():
 	if get_parent().name == "Red Tiles":
 		sprite.set_texture(red)
 		score = 20
-	if get_parent().name == "Blue Tiles":
-		sprite.set_texture(blue)
-		score = 30
 	if get_parent().name == "Green Tiles":
 		sprite.set_texture(green)
 		score = 40
